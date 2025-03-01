ac4c1d299c3f68507244a8a3e769d8f308bc828c
Amir Kooshky <kooshkya@gmail.com>
2025-03-01 12:59:38 +0330

    create boxes

--- cards_app/admin.py
+++ cards_app/admin.py
@@ -4,10 +4,9 @@ from .models import Flashcard, Review
 
 @admin.register(Flashcard)
 class FlashcardAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created_at', 'updated_at')  # Show timestamps in the list view
+    list_display = ('id', 'box', 'created_at', 'updated_at') 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'flashcard', 'review_date', 'remembered')  # Ensure review_date is visible
-    ordering = ('-review_date',)  # Show latest reviews first
-    list_filter = ('review_date', 'remembered')  # Add filtering options
+    list_display = ('id', 'flashcard', 'review_date', 'remembered') 
+    list_filter = ('review_date', 'remembered')
--- cards_app/models.py
+++ cards_app/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+from datetime import timedelta
+
+LEITNER_REVIEW_PERIODS = {
+    1: timedelta(days=1),  
+    2: timedelta(days=2),  
+    3: timedelta(days=5),  
+    4: timedelta(days=10),  
+    5: timedelta(days=30)
+}
 
 class Flashcard(models.Model):
+    BOX_CHOICES = [(i, str(i)) for i in range(1, 6)]
+
     front = RichTextField() 
     back = RichTextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    box = models.IntegerField(choices=BOX_CHOICES, default=1)
+
     def __str__(self):
-        return f"Flashcard {self.id}"
+        return f"Flashcard {self.id} (Box {self.box})"
     
 
 class Review(models.Model):
